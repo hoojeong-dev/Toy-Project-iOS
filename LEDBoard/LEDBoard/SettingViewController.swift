3f79f86a38ec5998456a5295dc33a28be708de6a
gnwjd309 <fujeong15@naver.com>
2022-03-15 17:03:22 +0900

    Add change value setting function

--- LEDBoard/LEDBoard/SettingViewController.swift
+++ LEDBoard/LEDBoard/SettingViewController.swift
@@ -7,12 +7,16 @@
 
 import UIKit
 
+protocol LEDBoardSettingDelegate: AnyObject {
+    func changedSetting(text: String?, textColor: UIColor, backgroundColor: UIColor)
+}
+
 class SettingViewController: UIViewController {
 
     
     @IBOutlet weak var textField: UITextField!
     
-    // 사용자가 색상 버튼을 선택하면, 선택한 버튼의 알파 겂은 1로, 나머지의 알파 값은 0.2로 변경한다.
+    // 사용자가 색상 버튼을 선택하면, 선택한 버튼의 알파 겂은 1로, 나머지의 알파 값은 0.2로 변경
     @IBOutlet weak var yellowButton: UIButton!
     @IBOutlet weak var purpleButton: UIButton!
     @IBOutlet weak var greenButton: UIButton!
@@ -21,10 +25,32 @@ class SettingViewController: UIViewController {
     @IBOutlet weak var blueButton: UIButton!
     @IBOutlet weak var orangeButton: UIButton!
     
+    // delegate 프로퍼티 추가
+    weak var delegate: LEDBoardSettingDelegate?
+    
+    // 사용자가 입력한 문자를 저장하는 프로퍼티
+    var ledText: String?
+    
+    // 설정된 값을 changedSetting 메서드에 전달하기 위해 텍스트와 배경 컬러 프로퍼티를 추가
+    // 사용자가 버튼을 선택할 때마다 이 프로퍼티에 색이 저장됨
+    // 기본 값은 각각 yellow, black로 초기화
+    var textColor: UIColor = .yellow
+    var backgroundColor: UIColor = .black
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.configureView()
     }
     
+    // 전달 받은 값으로 뷰를 초기화
+    // 색을 바꾸고 전광판으로 돌아갔다가 설정창으로 오면, 내가 바꿔놨던 색으로 선택되어있지 않기 때문
+    private func configureView() {
+        if let ledText = self.ledText {
+            self.textField.text = ledText
+        }
+        self.changeTextColor(color: self.textColor)
+        self.changeBackgroundColor(color: self.backgroundColor)
+    }
     // 액션 함수를 하나만 설정해놓고, 다른 것들을 우클릭으로 연결해주면 됨
     // 누가 클릭했는지는 sender 파라미터로 알 수 있음. (선택하면 선택한 버튼의 인스턴스를 전달하기 때문 )
     @IBAction func tapTextColorButton(_ sender: UIButton) {
@@ -32,25 +58,37 @@ class SettingViewController: UIViewController {
         // sender와 버튼 아울렛 변수를 비교하면 된다.
         if sender == self.yellowButton {
             self.changeTextColor(color: .yellow)
+            self.textColor = .yellow
         } else if sender == self.purpleButton {
             self.changeTextColor(color: .purple)
+            self.textColor = .purple
         } else {
             self.changeTextColor(color: .green)
+            self.textColor = .green
         }
     }
     
     @IBAction func tapBackgroundColorButton(_ sender: UIButton) {
         if sender == self.blackButton {
             self.changeBackgroundColor(color: .black)
+            self.backgroundColor = .black
         } else if sender == self.blueButton {
             self.changeBackgroundColor(color: .blue)
+            self.backgroundColor = .blue
         } else {
             self.changeBackgroundColor(color: .orange)
+            self.backgroundColor = .orange
         }
     }
     
     @IBAction func tapSaveButton(_ sender: UIButton) {
+        // 저장 버튼을 누르면, changedSetting 메서드를 호출하여 선택된 색상을 넘겨줌
+        self.delegate?.changedSetting(text: self.textField.text,
+                                      textColor: textColor,
+                                      backgroundColor: backgroundColor)
         
+        // 설정한 뒤, 이전 화면으로 이동
+        self.navigationController?.popViewController(animated: true)
     }
     
     private func changeTextColor(color: UIColor) {
--- LEDBoard/LEDBoard/ViewController.swift
+++ LEDBoard/LEDBoard/ViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, LEDBoardSettingDelegate {
 
     @IBOutlet weak var contentsLabel: UILabel!
     
@@ -18,6 +18,33 @@ class ViewController: UIViewController {
         self.contentsLabel.textColor = .yellow
     }
 
-
+    // Segue로 구현했기 때문에 prepare메서드 사용
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // segue.destination를 사용해 SettingViewController의 인스턴스를 가져온 뒤,
+        // SettingViewController로 다운캐스팅
+        if let settingViewController = segue.destination as? SettingViewController {
+            // SettingViewController의 delegate로 위임받기
+            settingViewController.delegate = self
+            
+            // 색을 변경한 뒤, 다시 설정 창으로 넘어갈 때 변경된 값으로 선택되어 있도록 변경한 값을 다시 넘겨줌
+            settingViewController.ledText = self.contentsLabel.text
+            settingViewController.textColor = self.contentsLabel.textColor
+            // 만약 옵셔널이라면(nil) black로 설정
+            settingViewController.backgroundColor = self.view.backgroundColor ?? .black
+        }
+    }
+    
+    func changedSetting(text: String?, textColor: UIColor, backgroundColor: UIColor) {
+        // 이전 화면에서 전달하는 색상 값들로 뷰를 초기화
+        if let text = text {
+            self.contentsLabel.text = text
+        }
+        
+        self.contentsLabel.textColor = textColor
+        self.view.backgroundColor = backgroundColor
+        
+        // 이렇게 하면 색상을 변경한 뒤, 저장을 하면 뷰에 정상적으로 표시되지만
+        // 다시 설정 뷰로 돌아가면 바뀐 색상으로 나타나지 않음
+        // 그래서 설정 뷰로 넘어갈 때 값을 변경해주는 코드가 필요함
+    }
 }
-

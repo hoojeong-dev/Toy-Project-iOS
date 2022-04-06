ad9c0cf1cfd4d88083e071bd4821b326854668f9
gnwjd309 <fujeong15@naver.com>
2022-04-06 16:16:26 +0900

    Implement covid status chart

--- COVID19/COVID19/ViewController.swift
+++ COVID19/COVID19/ViewController.swift
@@ -22,7 +22,9 @@ class ViewController: UIViewController {
             
             switch result {
             case let .success(result):
-                debugPrint("success \(result)")
+                self.configureStackView(koreaCovidOverview: result.korea)
+                let covidOverviewList = self.makeCovidOverviewList(cityCovidOverview: result)
+                self.configureChartView(covidOverviewList: covidOverviewList)
             
             case let .failure(error):
                 debugPrint("error \(error)")
@@ -30,12 +32,15 @@ class ViewController: UIViewController {
         })
     }
 
+    
+    
+    // api 호출
     func fetchCovidOverview(
         completionHandler: @escaping (Result<CityCovidOverview, Error>) -> Void
     ) {
         let url = "https://api.corona-19.kr/korea/country/new/"
         let param = [
-            "serviceKey": "corona-api-key"
+            "serviceKey": "covid-api-key"
         ]
         
         AF.request(url, method: .get, parameters: param)
@@ -56,6 +61,72 @@ class ViewController: UIViewController {
                 
             })
     }
-
+    
+    
+    
+    // 확진자 수 표기 메서드
+    func configureStackView(koreaCovidOverview: CovidOverView) {
+        self.totalCaseLabel.text = "\(koreaCovidOverview.totalCase)명"
+        self.newCaseLabel.text = "\(koreaCovidOverview.newCase)명"
+    }
+    
+    
+    
+    // pieChart 구성 메서드
+    func configureChartView(covidOverviewList: [CovidOverView]) {
+        // 표시할 도시 데이터를 pieChart 데이터 엔트리 객체로 매핑
+        let entries = covidOverviewList.compactMap { [weak self] overview -> PieChartDataEntry? in
+            guard let self = self else { return nil }
+            
+            return PieChartDataEntry(value: self.removeFormatString(string: overview.newCase), label: overview.countryName, data: overview)
+        }
+        
+        let dataSet = PieChartDataSet(entries: entries, label: "코로나 발생 현황")
+        
+        dataSet.sliceSpace = 1
+        dataSet.entryLabelColor = .black
+        dataSet.valueTextColor = .black
+        dataSet.xValuePosition = .outsideSlice
+        dataSet.valueLinePart1OffsetPercentage = 0.8
+        dataSet.valueLinePart1Length = 0.2
+        dataSet.valueLinePart2Length = 0.3
+        
+        dataSet.colors = ChartColorTemplates.vordiplom() +
+            ChartColorTemplates.joyful() +
+            ChartColorTemplates.liberty() +
+            ChartColorTemplates.pastel() +
+            ChartColorTemplates.material()
+        
+        self.pieChartView.data = PieChartData(dataSet: dataSet)
+        self.pieChartView.spin(duration: 0.5, fromAngle: self.pieChartView.rotationAngle, toAngle: self.pieChartView.rotationAngle + 80)
+    }
+    
+    // String을 Double 타입으로 변환하는 메서드
+    func removeFormatString(string: String) -> Double {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .decimal
+        
+        return formatter.number(from: string)?.doubleValue ?? 0
+    }
+    
+    // pieChart에 표시하기 위한 도시 리스트 반환 메서드
+    func makeCovidOverviewList(cityCovidOverview: CityCovidOverview) -> [CovidOverView] {
+        return [
+            cityCovidOverview.seoul,
+            cityCovidOverview.busan,
+            cityCovidOverview.daegu,
+            cityCovidOverview.incheon,
+            cityCovidOverview.gwangju,
+            cityCovidOverview.daejeon,
+            cityCovidOverview.ulsan,
+            cityCovidOverview.sejong,
+            cityCovidOverview.gyeonggi,
+            cityCovidOverview.gyeongbuk,
+            cityCovidOverview.gyeongnam,
+            cityCovidOverview.chungbuk,
+            cityCovidOverview.chungnam,
+            cityCovidOverview.jeju
+        ]
+    }
 }
 

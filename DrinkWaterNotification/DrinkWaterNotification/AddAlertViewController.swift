046ebc51acf0acb9993856dc1e7b9b9a5fce737e
gnwjd309 <fujeong15@naver.com>
2022-05-11 19:06:41 +0000

    Implement UserNotification

--- DrinkWaterNotification/DrinkWaterNotification/AddAlertViewController.swift
+++ DrinkWaterNotification/DrinkWaterNotification/AddAlertViewController.swift
@@ -1,13 +1,46 @@
 import UIKit
 
+enum SetAlertMode {
+    case new
+    case edit(IndexPath, Alert)
+}
+
 class AddAlertViewController: UIViewController {
 
+    var id: String?
+    var setAlertMode: SetAlertMode = .new
+    
+    let userNotificationCenter = UNUserNotificationCenter.current()
+    
+    @IBOutlet weak var removeAlertButton: UIButton!
+    @IBOutlet weak var titleNavigationBar: UINavigationBar!
     @IBOutlet weak var datePicker: UIDatePicker!
+    
     var datePicked: ((_ date:Date) -> Void)?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        self.configureView()
+        self.configureEditMode()
+    }
+    
+    private func configureView() {
+        self.navigationItem.title = "알람 추가"
+        self.removeAlertButton.isHidden = false
+    }
+    
+    private func configureEditMode() {
+        switch self.setAlertMode {
+        case let .edit(_, alert):
+            self.id = alert.id
+            // datepicker에 나타나는 시간을 기존에 저장한 시간으로 바꾸는 코드 추가
+            self.navigationItem.title = "알람 추가"
+            self.removeAlertButton.isHidden = true
+        
+        default:
+            break
+        }
     }
     
     @IBAction func tapCancelButton(_ sender: UIBarButtonItem) {
@@ -15,8 +48,21 @@ class AddAlertViewController: UIViewController {
     }
     
     @IBAction func tapSaveButton(_ sender: UIBarButtonItem) {
-        datePicked?(datePicker.date)
+        switch self.setAlertMode {
+        case .new:
+            datePicked?(datePicker.date)
+            
+        case let .edit(_, alert):
+            let date = self.datePicker.date
+            let editAlert = Alert(id: alert.id, date: date, isOn: true)
+            NotificationCenter.default.post(name: NSNotification.Name("edit"), object: editAlert, userInfo: nil)
+        }
+
         super.dismiss(animated: true, completion: nil)
     }
     
+    @IBAction func tapRemoveButton(_ sender: UIButton) {
+        NotificationCenter.default.post(name: NSNotification.Name("delete"), object: id, userInfo: nil)
+        super.dismiss(animated: true, completion: nil)
+    }
 }
--- DrinkWaterNotification/DrinkWaterNotification/AppDelegate.swift
+++ DrinkWaterNotification/DrinkWaterNotification/AppDelegate.swift
@@ -1,36 +1,88 @@
-//
-//  AppDelegate.swift
-//  DrinkWaterNotification
-//
-//  Created by 김후정 on 2022/06/08.
-//
-
 import UIKit
+import CoreData
+import UserNotifications
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    let userNotificationCenter = UNUserNotificationCenter.current()
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        userNotificationCenter.delegate = self
+        
+        let authorizationOptions = UNAuthorizationOptions(arrayLiteral: [.badge, .sound, .alert])
+        userNotificationCenter.requestAuthorization(options: authorizationOptions) { _, error in
+            if let error = error {
+                print("Error: notification authorization request: \(error.localizedDescription)")
+            }
+        }
         return true
     }
 
-    // MARK: UISceneSession Lifecycle
 
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
+
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
 
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+
     }
 
+    /*
+     The persistent container for the application. This implementation
+     creates and returns a container, having loaded the store for the
+     application to it. This property is optional since there are legitimate
+     error conditions that could cause the creation of the store to fail.
+    */
+    lazy var persistentContainer: NSPersistentContainer = {
+        
+        let container = NSPersistentContainer(name: "DrinkWater")
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error as NSError? {
+                // Replace this implementation with code to handle the error appropriately.
+                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+                 
+                /*
+                 Typical reasons for an error here include:
+                 * The parent directory does not exist, cannot be created, or disallows writing.
+                 * The persistent store is not accessible, due to permissions or data protection when the device is locked.
+                 * The device is out of space.
+                 * The store could not be migrated to the current model version.
+                 Check the error message to determine what the actual problem was.
+                 */
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+        return container
+    }()
+
+    // MARK: - Core Data Saving support
+
+    func saveContext () {
+        let context = persistentContainer.viewContext
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                // Replace this implementation with code to handle the error appropriately.
+                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+                let nserror = error as NSError
+                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
+        }
+    }
 
 }
 
+extension AppDelegate: UNUserNotificationCenterDelegate {
+
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        completionHandler([.banner, .list, .sound, .badge])
+    }
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+        completionHandler()
+    }
+}

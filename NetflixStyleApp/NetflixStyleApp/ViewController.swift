4546d50e6dc12ded34ed9e1cc324f0c0af55cd05
gnwjd309 <fujeong15@naver.com>
2022-05-22 14:58:07 +0000

    Upload NetflixStyleApp

---
+++ NetflixStyleApp/NetflixStyleApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NefliexStyleApp
+//
+//  Created by 김후정 on 2022/06/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

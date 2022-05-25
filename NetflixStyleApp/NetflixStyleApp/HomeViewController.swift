2385128450055efd8fdbf32d9026d9ec47837d95
gnwjd309 <fujeong15@naver.com>
2022-05-25 18:19:23 +0000

    Add CollectionView

--- NetflixStyleApp/NetflixStyleApp/HomeViewController.swift
+++ NetflixStyleApp/NetflixStyleApp/HomeViewController.swift
@@ -9,4 +9,89 @@ import UIKit
 
 class HomeViewController: UICollectionViewController {
     
+    var contents: [Content] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.initNavigationBar()
+        
+        contents = getContents()
+        
+        //CollectionView Item(Cell) 설정
+        collectionView.register(ContentCollectionViewCell.self, forCellWithReuseIdentifier: "ContentCollectionViewCell")
+        collectionView.register(ContentCollectionViewHeader.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: "ContentCollectionViewHeader")
+    }
+    
+    // 네비게이션 설정
+    func initNavigationBar() {
+        navigationController?.navigationBar.backgroundColor = .clear
+        navigationController?.navigationBar.setBackgroundImage(UIImage(), for: .default)
+        navigationController?.navigationBar.shadowImage = UIImage()
+        navigationController?.hidesBarsOnSwipe = true
+        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(image: UIImage(named: "netflix_icon"), style: .plain, target: nil, action: nil)
+        navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "person.crop.circle"), style: .plain, target: nil, action: nil)
+    }
+    
+    // data 설정, 가져오기
+    func getContents() -> [Content] {
+        guard let path = Bundle.main.path(forResource: "Content", ofType: "plist"),
+              let data = FileManager.default.contents(atPath: path),
+              let list = try? PropertyListDecoder().decode([Content].self, from: data) else { return [] }
+        
+        return list
+    }
 }
+
+//UICollectionView DataSource, Delegate 성정
+extension HomeViewController {
+    // section 마다 보여질 셀의 개수
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        switch section {
+        // 메인화면 최상단에서 contents에 상관 없이 하나만을 보여줌
+        case 0:
+            return 1
+        // 나머지는 각각의 contentItem 배열의 개수(= row)만큼 나타냄
+        default:
+            return contents[section].contentItem.count
+        }
+    }
+    
+    // CollectionView 셀 설정
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        switch contents[indexPath.section].sectionType {
+        case .basic, .large:
+            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ContentCollectionViewCell", for: indexPath) as? ContentCollectionViewCell else { return UICollectionViewCell() }
+            cell.imageView.image = contents[indexPath.section].contentItem[indexPath.row].image
+            
+            return cell
+        default:
+            return UICollectionViewCell()
+        }
+    }
+    
+    // header 설정
+    override func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        if kind == UICollectionView.elementKindSectionHeader {
+            guard let headerView = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: "ContentCollectionViewHeader", for: indexPath) as? ContentCollectionViewHeader else { fatalError("Could not dequeue Header")}
+            headerView.sectionNameLabel.text = contents[indexPath.section].sectionName
+            
+            return headerView
+        } else {
+            return UICollectionReusableView()
+        }
+    }
+    
+    // section 개수 설정
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return contents.count
+    }
+    
+    //셀 선택
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let sectionName = contents[indexPath.section].sectionName
+        print("TEST: \(sectionName), \(indexPath.row + 1)th contents")
+    }
+}
+

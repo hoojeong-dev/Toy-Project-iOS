cb4cfe762bf7b16bcccf16da8e1bf0e61761da3a
gnwjd309 <fujeong15@naver.com>
2022-06-03 17:38:03 +0000

    Add header, basic section layout

--- NetflixStyleApp/NetflixStyleApp/HomeViewController.swift
+++ NetflixStyleApp/NetflixStyleApp/HomeViewController.swift
@@ -15,13 +15,18 @@ class HomeViewController: UICollectionViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // 초기 설정
         self.initNavigationBar()
-        
         contents = getContents()
+        
+        collectionView.backgroundColor = .black
         
         //CollectionView Item(Cell) 설정
         collectionView.register(ContentCollectionViewCell.self, forCellWithReuseIdentifier: "ContentCollectionViewCell")
         collectionView.register(ContentCollectionViewHeader.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: "ContentCollectionViewHeader")
+        
+        //section 설정
+        collectionView.collectionViewLayout = layout()
     }
     
     // 네비게이션 설정
@@ -43,20 +48,73 @@ class HomeViewController: UICollectionViewController {
         
         return list
     }
+    
+    // 각 sectionType에 대한 UICollectionViewLayout 생성
+    private func layout() -> UICollectionViewLayout {
+        return UICollectionViewCompositionalLayout {[weak self]
+            sectionNumber, environment ->NSCollectionLayoutSection? in
+            guard let self = self else { return nil }
+            
+            switch self.contents[sectionNumber].sectionType {
+            case .basic:
+                return self.createBasicTypeSection()
+            default:
+                return nil
+            }
+        }
+    }
+    
+    
+    // basic section layout 설정
+    private func createBasicTypeSection() -> NSCollectionLayoutSection {
+        // item
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.3), heightDimension: .fractionalHeight(0.75))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        item.contentInsets = .init(top: 10, leading: 5, bottom: 0, trailing: 5)
+        
+        // group
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.9), heightDimension: .estimated(200))
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitem: item, count: 3)
+        
+        // section
+        let section = NSCollectionLayoutSection(group: group)
+        section.orthogonalScrollingBehavior = .continuous
+        section.contentInsets = .init(top: 0, leading: 5, bottom: 0, trailing: 5)
+        
+        // header
+        let sectionHeader = self.createSectionHeader()
+        section.boundarySupplementaryItems = [sectionHeader]
+        
+        return section
+    }
+    
+    // header section layout 설정
+    private func createSectionHeader() -> NSCollectionLayoutBoundarySupplementaryItem {
+        //section header size
+        let layoutSectionHeaderSzie = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .absolute(30))
+        
+        //section header layout
+        let sectionHeader = NSCollectionLayoutBoundarySupplementaryItem(layoutSize: layoutSectionHeaderSzie, elementKind: UICollectionView.elementKindSectionHeader, alignment: .top)
+        
+        return sectionHeader
+    }
 }
 
 //UICollectionView DataSource, Delegate 성정
 extension HomeViewController {
     // section 마다 보여질 셀의 개수
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        switch section {
-        // 메인화면 최상단에서 contents에 상관 없이 하나만을 보여줌
-        case 0:
-            return 1
-        // 나머지는 각각의 contentItem 배열의 개수(= row)만큼 나타냄
-        default:
-            return contents[section].contentItem.count
+        if contents[section].sectionType == .basic {
+            switch section {
+            // 메인화면 최상단에서 contents에 상관 없이 하나만을 보여줌
+            case 0:
+                return 1
+            // 나머지는 각각의 contentItem 배열의 개수(= row)만큼 나타냄
+            default:
+                return contents[section].contentItem.count
+            }
         }
+        return 0
     }
     
     // CollectionView 셀 설정

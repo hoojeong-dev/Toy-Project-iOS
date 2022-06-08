f02cb8c5d7bf6b48d6ad3ef54232e020a6ecb79c
gnwjd309 <fujeong15@naver.com>
2022-06-08 18:05:27 +0000

    Add rank section layout

--- NetflixStyleApp/NetflixStyleApp/HomeViewController.swift
+++ NetflixStyleApp/NetflixStyleApp/HomeViewController.swift
@@ -24,6 +24,7 @@ class HomeViewController: UICollectionViewController {
         //CollectionView Item(Cell) 설정
         collectionView.register(ContentCollectionViewCell.self, forCellWithReuseIdentifier: "ContentCollectionViewCell")
         collectionView.register(ContentCollectionViewHeader.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: "ContentCollectionViewHeader")
+        collectionView.register(ContentCollectionViewRankCell.self, forCellWithReuseIdentifier: "ContentCollectionViewRankCell")
         
         //section 설정
         collectionView.collectionViewLayout = layout()
@@ -60,6 +61,8 @@ class HomeViewController: UICollectionViewController {
                 return self.createBasicTypeSection()
             case .large:
                 return self.createLargeTypeSection()
+            case .rank:
+                return self.createRankTypeSection()
             default:
                 return nil
             }
@@ -92,6 +95,29 @@ class HomeViewController: UICollectionViewController {
     
     // large section layout 설정
     private func createLargeTypeSection() -> NSCollectionLayoutSection {
+        // item
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.3), heightDimension: .fractionalHeight(0.9))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        item.contentInsets = .init(top: 10, leading: 5, bottom: 0, trailing: 5)
+        
+        // group
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.9), heightDimension: .estimated(200))
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitem: item, count: 2)
+        
+        // section
+        let section = NSCollectionLayoutSection(group: group)
+        section.orthogonalScrollingBehavior = .continuous
+        section.contentInsets = .init(top: 0, leading: 5, bottom: 0, trailing: 5)
+        
+        // header
+        let sectionHeader = self.createSectionHeader()
+        section.boundarySupplementaryItems = [sectionHeader]
+        
+        return section
+    }
+    
+    // rank section layout 설정
+    private func createRankTypeSection() -> NSCollectionLayoutSection {
         // item
         let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.3), heightDimension: .fractionalHeight(0.75))
         let item = NSCollectionLayoutItem(layoutSize: itemSize)
@@ -129,7 +155,7 @@ class HomeViewController: UICollectionViewController {
 extension HomeViewController {
     // section 마다 보여질 셀의 개수
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        if contents[section].sectionType == .basic || contents[section].sectionType == .large {
+        if contents[section].sectionType == .basic || contents[section].sectionType == .large || contents[section].sectionType == .rank {
             switch section {
             // 메인화면 최상단에서 contents에 상관 없이 하나만을 보여줌
             case 0:
@@ -147,7 +173,15 @@ extension HomeViewController {
         switch contents[indexPath.section].sectionType {
         case .basic, .large:
             guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ContentCollectionViewCell", for: indexPath) as? ContentCollectionViewCell else { return UICollectionViewCell() }
+            
             cell.imageView.image = contents[indexPath.section].contentItem[indexPath.row].image
+            
+            return cell
+        case .rank:
+            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ContentCollectionViewRankCell", for: indexPath) as? ContentCollectionViewRankCell else { return UICollectionViewCell() }
+            
+            cell.imageView.image = contents[indexPath.section].contentItem[indexPath.row].image
+            cell.rankLabel.text = String(describing: indexPath.row + 1)
             
             return cell
         default:

15b64cb0e9e7de3c4cade0e358581235bfaa5650
hoojeong-dev <58141862+hoojeong-dev@users.noreply.github.com>
2022-09-01 16:38:24 +0900

    chore: project 초기 설정

---
+++ GithubRepository/GithubRepository/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GithubRepository
+//
+//  Created by 김후정 on 2022/09/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
